b0d860f676f277e551bb27adfe05728c2ddf8fd6
gami0001 <gami0001@stud.kea.dk>
2025-10-19 13:43:35 +0200

    detlykkedes

--- JS/bliv.js
+++ JS/bliv.js
@@ -1,12 +1,14 @@
 const days = document.querySelectorAll(".day");
 const times = document.getElementById("times");
+const bookBtn = document.getElementById("bookBtn");
 
 days.forEach((day) => {
   day.addEventListener("click", () => {
     days.forEach((d) => d.classList.remove("selected"));
     day.classList.add("selected");
 
-    // Tilføj "visible"-klassen i stedet for at ændre display direkte
+    // Vis tider og knap
     times.classList.add("visible");
+    bookBtn.classList.add("visible");
   });
 });
